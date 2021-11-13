d0cee859dd09916a64dfcc1620b787f509a890fb
CostaMateus <costa.mack95@gmail.com>
2021-11-13 11:08:33 -0300

    basic configs

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware(["auth"])->group(function () {
+
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
