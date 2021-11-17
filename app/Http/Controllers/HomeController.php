6708bc03699a7f64d5e2666b2126f1819bebdbf9
CostaMateus <costa.mack95@gmail.com>
2021-11-17 15:41:38 -0300

    Ajuste fluxo de infos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use PhpParser\Node\Expr\FuncCall;
 
 class HomeController extends Controller
 {
@@ -24,5 +25,38 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function treatments()
+    {
+        $treatments = [
+            [
+                "title"       => "Clínico Geral",
+                "description" => "O dentista clínico geral é o profissional da saúde capacitado na área de odontologia, entre as suas competências estão prevenção, diagnósticos e tratamento de uma ampla variedade de condições, desordens e doenças dos dentes e gengiva. O dentista clínico geral prestam serviços relacionados a manutenção de higiene oral e saúde bucal.",
+            ],[
+                "title"       => "Cirurgia",
+                "description" => "A cirurgia é a especialidade da Medicina Dentária que inclui o diagnóstico e o tratamento cirúrgico de lesões e defeitos, incluindo tanto os aspectos funcionais como os estéticos, dos tecidos moles e duros da região maxilofacial.\nNesta especialidade são englobados tratamentos como as conhecidos extrações dentárias, mas também ósseas ( tórus e exostoses, quistos, lesões tumorais, enxertos ósseos, etc) e nos tecidos moles (remoção de fibromas, sialolitos nas glândulas salivares, epúlides gengivais, freios linguais e labias que provocam limitações na fala e maloclusão, cirurgias pré-protéticas, drenagem de abcessos e outroas).",
+            ],[
+                "title"       => "3",
+                "description" => "3",
+            ],[
+                "title"       => "4",
+                "description" => "4",
+            ],[
+                "title"       => "5",
+                "description" => "5",
+            ],[
+                "title"       => "6",
+                "description" => "6",
+            ],[
+                "title"       => "7",
+                "description" => "7",
+            ],[
+                "title"       => "8",
+                "description" => "8",
+            ],
+        ];
+
+        return view("patient.our_treatments", compact(["treatments"]));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,42 +3,17 @@
 @section('title', 'AdminLTE')
 
 @section('content_header')
-    <h1 class="m-0 text-dark">Nossos Tratamentos</h1>
+    <h1 class="m-0 text-dark">Dashboard</h1>
 @stop
 
 @section('content')
     <div class="row">
-        <div class="col-lg-3 col-12">
-            <a href="#" data-toggle="modal" data-target="#modal-default">
-                <div class="small-box bg-white">
-                    <div class="inner text-center">
-                        <img class="img-circle elevation-2 mb-2" src="{{asset('images/home/s.png')}}" width="35" alt="Tratamento">
-                        <h4 class="mb-0 text-gray-dark">Cirurgia</h4>
-                        <span class="btn-link">saiba mais</span>
-                    </div>
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <p class="mb-0">You are logged in!</p>
                 </div>
-            </a>
+            </div>
         </div>
     </div>
-    <div class="modal fade show" id="modal-default" style="display: block; padding-right: 17px;" aria-modal="true" role="dialog">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h4 class="modal-title">Título</h4>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">×</span>
-              </button>
-            </div>
-            <div class="modal-body">
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-              <p>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            </div>
-            <div class="modal-footer float-right">
-              <button type="button" class="btn btn-primary btn-ocorp">Tenho interesse</button>
-            </div>
-          </div>
-          <!-- /.modal-content -->
-        </div>
-        <!-- /.modal-dialog -->
-      </div>
 @stop
