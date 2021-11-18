0923d3c30366667f10ce65551ae570b6776a6e8e
CostaMateus <costa.mack95@gmail.com>
2021-11-18 09:48:23 -0300

    Add menus principais Ajuste css cor oralcorp

--- config/adminlte.php
+++ config/adminlte.php
@@ -230,98 +230,40 @@ return [
     */
 
     'menu' => [
-        // Navbar items:
         [
-            'type'         => 'navbar-search',
-            'text'         => 'search',
-            'topnav_right' => true,
+            'text' => 'Nossos tratamentos',
+            'url'  => '/tratamentos',
+            'icon' => 'fas fa-fw fa-1x fa-tooth',
         ],
         [
-            'type'         => 'fullscreen-widget',
-            'topnav_right' => true,
-        ],
-
-        // Sidebar items:
-        [
-            'type' => 'sidebar-menu-search',
-            'text' => 'search',
+            'text' => 'Nossos contatos',
+            'url'  => '/contatos',
+            'icon' => 'fas fa-fw fa-1x fa-phone-alt',
         ],
         [
-            'text' => 'blog',
-            'url'  => 'admin/blog',
-            'can'  => 'manage-blog',
+            'text' => 'Agenda',
+            'url'  => '/agenda',
+            'icon' => 'fas fa-fw fa-1x fa-calendar-day',
         ],
         [
-            'text'        => 'pages',
-            'url'         => 'admin/pages',
-            'icon'        => 'far fa-fw fa-file',
-            'label'       => 4,
-            'label_color' => 'success',
-        ],
-        ['header' => 'account_settings'],
-        [
-            'text' => 'profile',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-user',
+            'text' => 'Meus sorrisos',
+            'url'  => '/meus-sorrisos',
+            'icon' => 'fas fa-fw fa-1x fa-laugh',
         ],
         [
-            'text' => 'change_password',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-lock',
+            'text' => 'Financeiro',
+            'url'  => '/financeiro',
+            'icon' => 'fas fa-fw fa-1x fa-dollar-sign',
         ],
         [
-            'text'    => 'multilevel',
-            'icon'    => 'fas fa-fw fa-share',
-            'submenu' => [
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-                [
-                    'text'    => 'level_one',
-                    'url'     => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url'  => '#',
-                        ],
-                        [
-                            'text'    => 'level_two',
-                            'url'     => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-            ],
-        ],
-        ['header' => 'labels'],
-        [
-            'text'       => 'important',
-            'icon_color' => 'red',
-            'url'        => '#',
+            'text' => 'Indique um amigo',
+            'url'  => '/indique',
+            'icon' => 'fas fa-fw fa-1x fa-comment-dots',
         ],
         [
-            'text'       => 'warning',
-            'icon_color' => 'yellow',
-            'url'        => '#',
-        ],
-        [
-            'text'       => 'information',
-            'icon_color' => 'cyan',
-            'url'        => '#',
+            'text' => 'Check-in',
+            'url'  => '/chekin',
+            'icon' => 'fas fa-fw fa-1x fa-map-marker-alt',
         ],
     ],
 
