527e58f452c167b898a121221b6c95ce715eaed5
Filipe Ferreira <filipregunta@hotmail.com>
2021-11-24 16:16:34 -0300

    Mais algumas mudanças no blade schedule

--- resources/views/patient/schedule.blade.php
+++ resources/views/patient/schedule.blade.php
@@ -10,21 +10,26 @@
 @stop
 
 @section('content')
-<div class="container-fluid">
 
-    <div class="card h-100 w-100 mb-0">
+    <div id="card-oc" class="row pt-3">
         <div class="row">
            @foreach ( $appointments as $key => $apt )
                 @if ( !empty($apt["date"]))
-                    <div class="col-w-75 col-md-8 py-4 px-5 mx-auto">
-                        <h6>Data: {{$apt["date"]}}</h6>
-                        <h6>Horário: {{$apt["time"]}}</h6>
+                <div class="col-12 col-sm-6 col-md-4 col-xl-3 @if($key % 2 ==0)
+                bg-light @else bg-dark @endif">
+                    <div class="my-auto py-3">
+                        <div div class="mx-0 mx-sm-auto text-sm-center float-left float-sm-none">
+                            <h6>Data: {{$apt["date"]}}</h6>
+                            <h6>Horário: {{$apt["time"]}}</h6>
+                        </div>
+                        <div class="mx-0 mx-sm-auto text-sm-center float-right float-sm-none">
+                            <button type="submit" class="btn btn-primary @if($key % 2 ==0)
+                            btn-oc @else btn-light @endif">Desmarcar</button>
+                        </div>
                     </div>
-                    <div class="col py-3 text-center">
-                        <button type="submit" class="btn btn-primary btn-oc">Desmarcar</button>
-                    </div>
+                </div>
                 @else
-                    <div class="col-w-75 col-md-8 py-4 px-5 mx-auto">
+                    <div class="col-w-100 col-md-8 py-4 px-5 mx-auto">
                         <h4>Você ainda não possui horário marcado.</h4>
                     </div>
                 @endif
@@ -32,7 +37,6 @@
         </div>
     </div>
 
-</div>
 @stop
 
 @include('patient.footer')
