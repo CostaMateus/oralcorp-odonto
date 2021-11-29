4f2255bd37258587baf5470150b78aed7704711b
CostaMateus <costa.mack95@gmail.com>
2021-11-29 08:44:38 -0300

    Usres, add campos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use PhpParser\Node\Expr\FuncCall;
 
 class HomeController extends Controller
 {
--- database/migrations/2021_11_01_000001_create_users_table.php
+++ database/migrations/2021_11_01_000001_create_users_table.php
@@ -13,13 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create("users", function (Blueprint $table) {
             $table->id();
             $table->integer("clinic_id")->unsigned()->index()->foreign()->references("id")->on("clinics");
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string("external_id")->default(0);
+            $table->string("name");
+            $table->string("full_name");
+            $table->string("email")->unique();
+            $table->string("phone")->nullable()->default(null);
+            $table->timestamp("email_verified_at")->nullable();
+            $table->string("password");
             $table->rememberToken();
             $table->timestamps();
         });
@@ -32,6 +35,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists("users");
     }
 }
