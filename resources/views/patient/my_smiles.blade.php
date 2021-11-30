cac7646ecdb8abd5a60b93fce0b1cf88cb41f4fa
Filipe Ferreira <filipregunta@hotmail.com>
2021-11-30 09:11:52 -0300

    Adicionada função de zoom nas fotos

--- resources/views/patient/my_smiles.blade.php
+++ resources/views/patient/my_smiles.blade.php
@@ -17,9 +17,11 @@
                 <div class="card-body">
                     <h3 class="w-100">Antes</h3>
                     @if (!empty($smile[0][0]["data"]))
-                        <!--<img src="$smile[0][0]["data"]" class="img-fluid w-50" alt="Sorriso depois">-->
-                        <img src="https://images-cdn.9gag.com/photo/a9ALB5j_700b.jpg" class="img-fluid w-50" alt="Sorriso antes">
-                    @else
+                        <a href="#" class="zoom">
+                            <!--<img src="$smile[0][0]["data"]" class="img-fluid w-50" alt="Sorriso depois">-->
+                            <img src="https://images-cdn.9gag.com/photo/a9ALB5j_700b.jpg" class="img-fluid w-50" alt="Sorriso antes">
+                        </a>
+                        @else
                         <h6 class="mb-4 text-muted">Imagem não cadastrada</h6>
                         <img src="{{ asset('images/no_image.png') }}" class="img-fluid w-50" alt="Sorriso antes">
                     @endif
@@ -32,8 +34,10 @@
                 <div class="card-body">
                     <h3 class="w-100">Depois</h3>
                     @if (!empty($smile[1][0]["data"]))
-                        <!--<img src="$smile[1][0]["data"]" class="img-fluid w-50" alt="Sorriso depois">-->
-                        <img src="https://www.42frases.com.br/wp-content/uploads/2018/10/frases-dr-house.jpg" class="img-fluid w-50" alt="Sorriso depois">
+                        <a href="#" class="zoom">
+                            <!--<img src="$smile[1][0]["data"]" class="img-fluid w-50" alt="Sorriso depois">-->
+                            <img src="https://www.42frases.com.br/wp-content/uploads/2018/10/frases-dr-house.jpg" class="img-fluid w-50" alt="Sorriso depois">
+                        </a>
                     @else
                         <h6 class="mb-4 text-muted">Imagem não cadastrada</h6>
                         <img src="{{ asset('images/no_image.png') }}" class="img-fluid w-50" alt="Sorriso depois">
@@ -41,8 +45,32 @@
                 </div>
             </div>
         </div>
+        
+        <div class="modal fade" id="imagemodal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog" data-dismiss="modal">
+              <div class="modal-content"  >
+                <div class="modal-body">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">X</span>
+                    </button>
+                    <img src="" class="imagepreview" style="width: 100%;" >
+                </div>
+              </div>
+            </div>
+          </div>
 
     </div>
+@stop
+
+@section('js')
+<script>
+    $(function() {
+        $('.zoom').on('click', function() {
+            $('.imagepreview').attr('src', $(this).find('img').attr('src'));
+            $('#imagemodal').modal('show');
+        });
+    });
+</script>
 @stop
 
 @include('patient.footer')
