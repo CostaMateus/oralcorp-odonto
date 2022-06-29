4d83b80e8b3d237cbaf7e76fafd3d8093df6b73a
CostaMateus <costa.mack95@gmail.com>
2022-06-29 18:09:18 -0300

    fix login de novo paciente (senha vazia)

--- app/Http/Controllers/PersonalEasyController.php
+++ app/Http/Controllers/PersonalEasyController.php
@@ -6,7 +6,9 @@ use App\Models\Role;
 use App\Models\User;
 use App\Models\Clinic;
 use App\Helpers\Helper;
+use Illuminate\Http\Request;
 use App\Services\PersonalEasyService;
+use App\Http\Requests\NewPasswordRequest;
 use App\Http\Requests\PersonalEasyRequest;
 
 class PersonalEasyController extends Controller
@@ -24,6 +26,24 @@ class PersonalEasyController extends Controller
         if ($response["code"] != 200) return self::redirectLogin($response["code"]);
 
         $login    = reset($response["data"]);
+
+        if (empty($login["password"]))
+        {
+            $tempPass = "oralcorp" . date("Y");
+            $userPass = strtolower($request->password);
+
+            if ($tempPass != $userPass) return self::redirectLogin(404);
+
+            session([
+                "external_id" => $login["external_id"],
+                "clinic_id"   => $request->clinic_id,
+                "email"       => $request->email,
+                "phone"       => $login["phone"],
+                "name"        => $login["name"],
+            ]);
+
+            return redirect("/newpass");
+        }
 
         if ($login["password"] != $request->password) return self::redirectLogin(404);
 
@@ -58,6 +78,47 @@ class PersonalEasyController extends Controller
         }
 
         if (!isset($user)) return self::redirectLogin(404);
+
+        auth()->loginUsingId($user->id);
+
+        session(["folder_view" => $user->roles->first()->slug]);
+
+        return redirect("/");
+    }
+
+    public function newPassword(Request $request)
+    {
+        return view("adminlte::auth.passwords.new");
+    }
+
+    public function savePassword(NewPasswordRequest $request)
+    {
+        $external_id = $request->external_id;
+        $clinic_id   = $request->clinic_id;
+        $password    = $request->password;
+        $phone       = $request->phone;
+        $email       = $request->email;
+        $name        = $request->name;
+
+        $clinic      = Clinic::find($clinic_id);
+
+        $response    = (new PersonalEasyService)->changePassword($password, $external_id, $clinic->code);
+
+        if ($response["code"] != 200) return self::redirectLogin($response["code"]);
+
+        $user = User::create([
+            "external_id" => $external_id,
+            "clinic_id"   => $clinic_id,
+            "email"       => $email,
+            "phone"       => $phone ?? null,
+            "name"        => explode(" ", $name)[0],
+            "full_name"   => $name,
+            "password"    => bcrypt($password . date("H:i:s")),
+        ]);
+
+        $role = Role::where("slug", "patient")->first();
+
+        $user->roles()->attach($role);
 
         auth()->loginUsingId($user->id);
 
